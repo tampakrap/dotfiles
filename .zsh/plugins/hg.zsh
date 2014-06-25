def9267cca7ebc2957e2a23e108627126ad948cd
Theo Chatzimichos <tampakrap@gmail.com>
2014-06-25 16:43:50 +0300

    [.zsh/plugins] Simplify code a bit, convert vars to global

--- .zsh/plugins/hg.zsh
+++ .zsh/plugins/hg.zsh
@@ -1,9 +1,5 @@
-function hg_prompt_symbol() {
-    hg root > /dev/null 2>&1 && echo '☿' && return
-}
-
 function hg_prompt_info() {
-    (hg prompt --angle-brackets "%{$fg[gray]%}$(hg_prompt_symbol)%{$reset_color%}$PROMPT_PREFIX%{$fg[white]%}<branch>$PROMPT_SUFFIX$PROMPT_PREFIX%{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}$PROMPT_SUFFIX<patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied()>") 2>/dev/null
+    (hg prompt --angle-brackets "☿%{$fg[green]%}[%{$reset_color%}<branch>%{$fg[green]%}][%{$reset_color%}%{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}%{$fg[green]%}]%{$reset_color%}<patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied()>") 2>/dev/null
 }
 
 function hg_prompt_modified() {
@@ -19,20 +15,20 @@ function hg_prompt_update() {
 }
 
 # If inside a Mercurial repository, print its branch, tag, state and patches
-function hg_prompt_string() {
+function hg_rprompt() {
     if [[ -d .hg  ]]; then
         hg_prompt="$(hg_prompt_info)"
         local HG_STATE
         local _state
         _state="$(hg_prompt_unknown)"
-        [ -n "$_state" ] && HG_STATE+="$PROMPT_UNTRACKED"
+        [ -n "$_state" ] && HG_STATE+="%{$fg_bold[red]%}●%{$reset_color%}"
         _state="$(hg_prompt_modified)"
-        [ -n "$_state" ] && HG_STATE+="$PROMPT_MODIFIED"
+        [ -n "$_state" ] && HG_STATE+="%{$fg_bold[yellow]%}●%{$reset_color%}"
         _state="$(hg_prompt_update)"
-        [ -n "$_state" ] && HG_STATE+="$PROMPT_STAGED"
-        [ -n "$HG_STATE" ] && hg_prompt+="$PROMPT_PREFIX$HG_STATE$PROMPT_SUFFIX"
+        [ -n "$_state" ] && HG_STATE+="%{$fg_bold[green]%}●%{$reset_color%}"
+        [ -n "$HG_STATE" ] && hg_prompt+="%{$fg[green]%}[%{$reset_color%}$HG_STATE%{$fg[green]%}]%{$reset_color%}"
         echo "$hg_prompt"
     fi
 }
 
-RPS1+='$(hg_prompt_string)'
+RPS1+='$(hg_rprompt)'
