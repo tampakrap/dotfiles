54139b206c2d0f08710442cafecbdfae11462311
Theo Chatzimichos <tampakrap@gmail.com>
2014-07-04 19:47:17 +0300

    [.zsh/plugins/keychain.zsh] Call `pass` in order to cache the gpg decr key

--- .zsh/plugins/keychain.zsh
+++ .zsh/plugins/keychain.zsh
@@ -1,4 +1,5 @@
 if [[ $(whoami) == ${KEYCHAIN_USER[1]} && ${WORKSTATIONS[@]} =~ $(hostname) ]]; then
     export ECHANGELOG_USER=${KEYCHAIN_USER[2]}
     eval `keychain --eval --nogui --timeout 180 id_rsa --quiet`
+    gpg2 -dq --compress-algo=none --batch ~/.password-store/opensuse.org/tampakrap.gpg > /dev/null
 fi
