73d70737976cd6a45fadd1e30df904e99da3a79f
Theo Chatzimichos <tampakrap@gmail.com>
2014-07-20 17:04:38 +0300

    .gnupg/gpg-agent.conf: Use pinentry-gtk-2 as default
    It falls back to pinentry-curses when no gui is available. I have created a
    wrapper for gpg2 to use pinentry-curses when $GPG_TTY exists
    
    .zsh/plugins/keychain.zsh: various/nothing produces no output, no need to
    redirect to /dev/null

--- .zsh/plugins/keychain.zsh
+++ .zsh/plugins/keychain.zsh
@@ -1,5 +1,5 @@
 if [[ $(whoami) == ${KEYCHAIN_USER[1]} && ${WORKSTATIONS[@]} =~ $(hostname) ]]; then
     export ECHANGELOG_USER=${KEYCHAIN_USER[2]}
-    eval `keychain --eval --nogui --timeout 180 id_rsa --quiet`
-    gpg2 -dq --compress-algo=none --batch ~/.password-store/various/nothing.gpg > /dev/null
+    eval `keychain --eval --nogui --quiet --timeout 180 id_rsa`
+    gpg2 -dq --batch --yes ~/.password-store/various/nothing.gpg
 fi
