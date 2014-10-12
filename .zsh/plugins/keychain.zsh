c9e7872769e639e4bd57e4b7f79a6c16d9bad86a
Theo Chatzimichos <tampakrap@gmail.com>
2014-10-12 23:36:14 +0300

    .zsh/plugins/keychain.zsh .gnupg/gpg-agent.conf: Increase the GPG Agent TTLs
    mainly due to heavy usage for authorization in mail fetching operations

--- .zsh/plugins/keychain.zsh
+++ .zsh/plugins/keychain.zsh
@@ -1,5 +1,5 @@
 if [[ $(whoami) == ${KEYCHAIN_USER[1]} && ${WORKSTATIONS[@]} =~ $(hostname) ]]; then
     export ECHANGELOG_USER=${KEYCHAIN_USER[2]}
-    eval `keychain --eval --nogui --quiet --timeout 180 id_rsa`
+    eval `keychain --eval --nogui --quiet --timeout 360 id_rsa`
     gpg2 -dq --batch --yes ~/.password-store/various/nothing.gpg
 fi
