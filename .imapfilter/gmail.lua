9eea17beaf98eeb7e77b986d6d0214c7d1711e14
Theo Chatzimichos <tampakrap@gmail.com>
2015-01-16 19:57:43 +0100

    .imapfilter: Move stuff from gmail to suse, add a bunch of filters in suse

--- .imapfilter/gmail.lua
+++ .imapfilter/gmail.lua
@@ -16,16 +16,7 @@ gmail_new = gmail['INBOX']:is_new()
 gmail_recent = gmail['INBOX']:is_recent()
 gmail_unseen = gmail['INBOX']:is_unseen()
 
-opensuse_list = function (list_prefix)
-    list = list_prefix .. '@opensuse.org'
-    list_mine = gmail_unseen:contain_from('tampakrap@opensuse.org')
-    return gmail_unseen:contain_field('X-Mailinglist', list_prefix) +
-           gmail_unseen:contain_to('tampakrap@opensuse.org'):contain_cc(list) +
-           gmail_unseen:contain_to('tampakrap@opensuse.org'):contain_to(list) +
-           list_mine:contain_to(list) + list_mine:contain_cc(list) +
-           gmail_unseen:match_from(list_prefix .. '\\+(owner|help)@opensuse.org')
-end
-
+gmail_unseen:match_field('X-BeenThere', '(google-summer-of-code|gsoc-mentors)-announce@googlegroups\\.com'):move_messages(gmail['lists/gsoc'])
 gmail_unseen:match_from('(accounts@oreilly|(invitation-)?do-not-reply@(trello|stack(exchange|overflow))|admin@transifex|reddit@reddit|no-reply@(dropbox|socghop\\.appspotmail|lanyrd|susestudio)|((d\\+MTA4MDQ2NTk2NjI3OTg3NjgyMjIw-.*@docs|no(-)?reply(@(accounts|checkout|cbg\\.bounces|wallet)|-.*@plus))\\.|(mail|drive)-noreply@)google|account-security-noreply@account\\.microsoft|notice-5622092@no-ip|gitlab@gitlab|ebay@ebay|(issue-updates|tickets|noreply)@puppetlabs|notification\\+ka4k2g4a@facebookmail|(miles\\.bonus|echeckin|noreply|boardingpass)@aegeanair|service@(intl\\.paypal|payu)|webmaster@novell|mantis@public\\.kitware|support@olx|donotreply@(gravatar|wordpress)|alerts@citibank|notify@twitter|noreply((-www@services\\.|@)swiss|youtube)|(info@advisory\\.|itinerary@)ryanair|(bme@mailer\\.|customer\\.service@)booking)\\.com|(notify@keybase|support@drone)\\.io|(support@cacert|accounts@fedoraproject|donotreply@rubygems|noreply@(redmine|wordpress)|no-reply@(persona|mozillians))\\.org|(no_reply@gopay|robot@jidloted|noreply@c(sas|inemacity)|eshop_noreply@dpp|obchod@ticketportal|ticket@ticketpro|letenky@pelikan|reservation@previo)\\.cz|no-reply@jetpack\\.me|(no-reply@vodafonecu|info@flowernet|billing@ziz)\\.gr|(fahrkartenservice@bahn|(info@|puppetcamp\\.teilnehmer@rt\\.)netways)\\.de'):move_messages(gmail['logs'])
 gmail_unseen:match_from('.*@bitbucket\\.org'):move_messages(gmail['logs/bitbucket'])
 github = gmail_unseen:match_from('((noreply|support)@github|notifications@travis-ci)\\.com') + gmail_unseen:contain_field('X-GitHub-Recipient', 'tampakrap')
@@ -33,16 +24,7 @@ github:move_messages(gmail['logs/github'])
 linkedin_all= gmail_unseen:match_from('.*@linkedin\\.com') + gmail_unseen:match_field('X-LinkedIn-Template', 'invite_guest_59|connection_suggestion_email')
 linkedin = linkedin_all - linkedin_all:contain_field('X-LinkedIn-Class', 'INMAIL')
 linkedin:move_messages(gmail['logs/linkedin'])
-gmail_unseen:match_field('X-OBS-event-type', 'build_.*'):move_messages(gmail['logs/opensuse/obs/builds'])
-obs_sr = gmail_unseen:match_field('X-OBS-event-type', 'comment_for_.*|request_.*') + gmail_unseen:contain_from('coolo@suse.de'):contain_subject('Reminder for openSUSE:Factory work')
-obs_sr:move_messages(gmail['logs/opensuse/obs/sr'])
-opensuse_redmine_admin = gmail_unseen:match_subject('.*\\[openSUSE admin - tickets #[0-9]{4}\\].*') + gmail_unseen:contain_field('X-Redmine-Project', 'opensuse-admin')
-opensuse_redmine_admin:move_messages(gmail['logs/opensuse/redmine/admin'])
-gmail_unseen:match_field('X-BeenThere', '(google-summer-of-code|gsoc-mentors)-announce@googlegroups\\.com'):move_messages(gmail['lists/gsoc'])
-opensuse_list('opensuse-announce'):move_messages(gmail['lists/opensuse'])
-opensuse_list('gsoc-mentors'):move_messages(gmail['lists/opensuse/gsoc-mentors'])
-opensuse_list('opensuse-cz'):move_messages(gmail['lists/opensuse/opensuse-cz'])
-opensuse_list('opensuse-el'):move_messages(gmail['lists/opensuse/opensuse-el'])
-gmail_unseen:match_from('(noreply@(mailer\\.atlassian|bitlysupport)|oreilly@post\\.oreilly|contact@puppetlabs|(Miles(&|\\+)Bonus|newsletter)@email\\.aegeanair|no-reply@dropboxmail|linkedin@e\\.linkedin|Microsoft@e-mail\\.microsoft|(alerts7|noreply3)@emails\\.skype|(news|support)@gitlab(control)?|info@smartwings|eBay@reply1\\.ebay|update\\+ka4k2g4a@facebookmail|(privacy-noreply|ingress-support)@google|e(-info|vents)@suse|invite@eventbrite|alerts@lanyrd|paypal@e\\.paypal)\\.com|(newsletter@arbes(dent|plus)|mailing@pelikan|(stuff|noreply)@linuxdays|info@installfest|novinky@jidloted|(newsletter@info\\.|directmail@)csa)\\.cz|((career|cs-news|carseniou)@teilar|(support|info|no-reply)@(ziz|papaki)|Webmaster@TheLab|noreply@(gtvs|eurobank)|specials@flowernet|info@ellak)\\.gr|((noreply|sales)@mailer\\.bitbucket|admin@fedoraproject|(events|no-reply)@linuxfoundation)\\.org|noreply@hetzner-status\\.de'):move_messages(gmail['newsletters'])
+gmail_unseen:match_from('(noreply@(mailer\\.atlassian|bitlysupport)|oreilly@post\\.oreilly|contact@puppetlabs|(Miles(&|\\+)Bonus|newsletter)@email\\.aegeanair|no-reply@dropboxmail|linkedin@e\\.linkedin|Microsoft@e-mail\\.microsoft|(alerts7|noreply3)@emails\\.skype|(news|support)@gitlab(control)?|info@smartwings|eBay@reply1\\.ebay|update\\+ka4k2g4a@facebookmail|(privacy-noreply|ingress-support)@google|e(-info|vents)@suse|invite@eventbrite|alerts@lanyrd|paypal@e\\.paypal)\\.com|(newsletter@arbes(dent|plus)|mailing@pelikan|(stuff|noreply)@linuxdays|info@installfest|novinky@jidloted|(newsletter@info\\.|directmail@)csa)\\.cz|((career|cs-news|carseniou)@teilar|(support|info|no-reply)@(ziz|papaki)|Webmaster@TheLab|noreply@(gtvs|eurobank)|specials@flowernet|(phplist-bounces@e|info@)ellak)\\.gr|((noreply|sales)@mailer\\.bitbucket|admin@fedoraproject|(events|no-reply)@linuxfoundation)\\.org|noreply@hetzner-status\\.de'):move_messages(gmail['newsletters'])
 gmail_unseen:match_from('(info@|vypis\\.citibank\\.cz@edelivery\\.)citi\\.com|obchodni\\.sdeleni@citiagent\\.cz'):move_messages(gmail['newsletters/citibank'])
+gmail_unseen:contain_from('faktury@developtech.cz'):move_messages(gmail['newsletters/developtech'])
 gmail_unseen:match_from('(fakturace|podzim)@pre\\.cz'):move_messages(gmail['newsletters/pre'])
--- .imapfilter/suse.lua
+++ .imapfilter/suse.lua
@@ -16,4 +16,44 @@ suse_new = suse['INBOX']:is_new()
 suse_recent = suse['INBOX']:is_recent()
 suse_unseen = suse['INBOX']:is_unseen()
 
-suse_unseen:match_field('X-OBS-event-type', 'build_.*'):move_messages(suse['logs/obs/builds'])
+
+opensuse_list = function (list_prefix)
+    list = list_prefix .. '@opensuse.org'
+    list_mine = suse_unseen:contain_from('tampakrap@opensuse.org')
+    return suse_unseen:contain_field('X-Mailinglist', list_prefix) +
+           suse_unseen:contain_to('tampakrap@opensuse.org'):contain_cc(list) +
+           suse_unseen:contain_to('tampakrap@opensuse.org'):contain_to(list) +
+           list_mine:contain_to(list) + list_mine:contain_cc(list) +
+           suse_unseen:match_from(list_prefix .. '\\+(owner|help)@opensuse.org')
+end
+
+
+suse_list = function (list_prefix, domain)
+    list = list_prefix .. '@suse\\.' .. domain
+    return suse_unseen:match_field('X-BeenThere', '^' .. list .. '$')
+end
+
+
+opensuse_list('opensuse-announce'):move_messages(suse['lists/opensuse'])
+opensuse_list('gsoc-mentors'):move_messages(suse['lists/opensuse/gsoc-mentors'])
+opensuse_list('opensuse-cz'):move_messages(suse['lists/opensuse/opensuse-cz'])
+opensuse_list('opensuse-el'):move_messages(suse['lists/opensuse/opensuse-el'])
+suse_list('cz', 'cz'):move_messages(suse['lists/suse/cz'])
+suse_list('opensuse-internal', 'de'):move_messages(suse['lists/suse/opensuse-internal'])
+suse_list('qa-maintenance', 'de'):move_messages(suse['lists/suse/qa-maintenance'])
+suse_list('qa-maintenance-dev', 'de'):move_messages(suse['lists/suse/qa-maintenance-dev'])
+suse_list('qa-maintenance-reports', 'de'):move_messages(suse['lists/suse/qa-maintenance-reports'])
+suse_list('patch-management', 'de'):move_messages(suse['lists/suse/patch-management'])
+suse_list('talk-cz', 'cz'):move_messages(suse['lists/suse/talk-cz'])
+logs = suse_unseen:match_from('GWAVA@novell\\.com') + suse_unseen:match_from('maint-coord@(novell\\.com|suse\\.de)'):match_subject('Maintenance QA (SLA|KPI) watchdog report - [0-9]{8}') + suse_unseen:match_field('RT-Ticket', 'SUSE Ticket #[0-9]{5,6}') + suse_unseen:contain_from('swamp_noreply@suse\\.de'):contain_to('tchatzimichos@suse.com')
+logs:move_messages(suse['logs'])
+ibs = suse_unseen:match_field('X-OBS-URL', 'https://build.suse.de')
+ibs:match_field('X-OBS-event-type', 'comment_for_.*|request_.*'):move_messages(suse['logs/ibs/sr'])
+ibs:match_field('X-OBS-event-type', 'build_.*'):move_messages(suse['logs/ibs/builds'])
+obs = suse_unseen:match_field('X-OBS-URL', 'https://build.opensuse.org')
+obs:match_field('X-OBS-event-type', 'build_.*'):move_messages(suse['logs/obs/builds'])
+obs_sr = obs:match_field('X-OBS-event-type', 'comment_for_.*|request_.*') + suse_unseen:contain_from('coolo@suse.de'):contain_subject('Reminder for openSUSE:Factory work')
+obs_sr:move_messages(suse['logs/obs/sr'])
+opensuse_redmine_admin = suse_unseen:match_subject('\\[openSUSE admin - tickets #[0-9]{4}\\].*') + suse_unseen:contain_field('X-Redmine-Project', 'opensuse-admin')
+opensuse_redmine_admin:move_messages(suse['logs/progress/opensuse-admin'])
+suse_unseen:match_to('DL-TAG-(ALL|EMPLOYEES)(\\.iList\\.INTERNET)?@ilist\\.attachmategroup\\.com'):move_messages(suse['newsletters'])
