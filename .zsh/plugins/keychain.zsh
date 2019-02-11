539abba71b97decee96f4cf0b88b139364cd73fb
Theo Chatzimichos <tampakrap@gmail.com>
2019-02-11 22:34:10 +0100

    .zsh/plugins/keychain.zsh: use hostname -s to not print the domain

--- .zsh/plugins/keychain.zsh
+++ .zsh/plugins/keychain.zsh
@@ -1,4 +1,4 @@
-if [[ $(whoami) == tampakrap && ${WORKSTATIONS[@]} =~ $(hostname) ]]; then
+if [[ $(whoami) == tampakrap && ${WORKSTATIONS[@]} =~ $(hostname -s) ]]; then
     eval `keychain --eval --nogui --quiet --timeout 360 --agents ssh,gpg id_ed25519 id_rsa`
     gpg2 -dq --batch --yes ~/.password-store/various/nothing.gpg
 fi
