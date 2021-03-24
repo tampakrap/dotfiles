31a07705c079217beafe009642328738d2111960
Theo Chatzimichos <tampakrap@gmail.com>
2021-03-24 20:52:29 +0100

    bump env vars

--- .zsh/plugins/jobandtalent.zsh
+++ .zsh/plugins/jobandtalent.zsh
@@ -1,5 +1,3 @@
-export VAULT_GITHUB_API_TOKEN=$(pass jobandtalent/github-token-jt-vault)
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+export VAULT_GITHUB_API_TOKEN=$(get-github-token-jt-vault)
 export TF_VAR_github_api_token=$VAULT_GITHUB_API_TOKEN
-export GPG_TTY=$(tty)
-export GPG_AGENT_INFO=${HOME}/.gnupg/S.agent\=
+#export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=yes
