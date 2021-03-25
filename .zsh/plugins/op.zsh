72902190127dd3ee56c2a9706c4fc79961035952
Theo Chatzimichos <tampakrap@gmail.com>
2021-03-25 09:29:56 +0100

    .zsh/plugins/op.zsh: Use a cache file for op completion.
    Generating the cache file each time the terminal starts is slow.

--- .zsh/plugins/op.zsh
+++ .zsh/plugins/op.zsh
@@ -1,1 +1,11 @@
-eval "$(op completion zsh)"; compdef _op op
+#eval "$(op completion zsh)"; compdef _op op
+
+__OP_COMPLETION_FILE="${HOME}/.cache/op_completion"
+if [[ -f $__OP_COMPLETION_FILE ]]; then
+    source $__OP_COMPLETION_FILE
+else
+    op completion zsh >! $__OP_COMPLETION_FILE
+fi
+unset __OP_COMPLETION_FILE
+
+compdef _op op
