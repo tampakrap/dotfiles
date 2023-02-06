938bb05e5fbe513e3f4108f0a3685ff26fed6c3a
Theo Chatzimichos <tampakrap@gmail.com>
2023-02-06 12:42:04 +0100

    .zsh/plugins/jobandtalent.zsh: use the new script to get token

--- .zsh/plugins/jobandtalent.zsh
+++ .zsh/plugins/jobandtalent.zsh
@@ -1,4 +1,4 @@
-export VAULT_GITHUB_API_TOKEN=$(get-github-token-jt-vault)
+export VAULT_GITHUB_API_TOKEN=$(get-token -t github-vault)
 export TF_VAR_github_api_token=$VAULT_GITHUB_API_TOKEN
 export REMOTE_USER=admin
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
