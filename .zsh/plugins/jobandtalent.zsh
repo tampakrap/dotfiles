6ee557d485c6e4400f5463d31d9fc878821094de
Theo Chatzimichos <tampakrap@gmail.com>
2023-02-06 21:01:31 +0100

    .zshrc: move the SAML2AWS_REGION to the work plugin

--- .zsh/plugins/jobandtalent.zsh
+++ .zsh/plugins/jobandtalent.zsh
@@ -2,6 +2,7 @@ export VAULT_GITHUB_API_TOKEN=$(get-token -t github-vault)
 export TF_VAR_github_api_token=$VAULT_GITHUB_API_TOKEN
 export REMOTE_USER=admin
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+export SAML2AWS_REGION=$(cat ${HOME}/.aws/default_region)
 export CLOUDFLARE_API_TOKEN=$(get-token -t cloudflare)
 #export BUNDLE_GEM__FURY__IO=$(get-token -t gemfury)
 #export TF_VAR_production_datadog_api_key=
