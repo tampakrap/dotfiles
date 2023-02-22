d2bbeb3114be0138ae71949c810116e7b4dc1b67
Theo Chatzimichos <tampakrap@gmail.com>
2023-02-22 13:17:14 +0100

    .zsh/plugins/terraform.zsh: don't automatically download new tf version

--- .zsh/plugins/terraform.zsh
+++ .zsh/plugins/terraform.zsh
@@ -1,2 +1,4 @@
+export TFENV_AUTO_INSTALL=false
+
 alias tfdocs="terraform-docs -c ~/.terraform-docs.yml md ."
 alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" '
