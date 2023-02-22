f02f02d52a44b22fbd3909bf5f24d63d930e7aa0
Theo Chatzimichos <tampakrap@gmail.com>
2023-02-22 13:17:51 +0100

    .zsh/plugins/terraform.zsh: Add tf alias

--- .zsh/plugins/terraform.zsh
+++ .zsh/plugins/terraform.zsh
@@ -1,4 +1,5 @@
 export TFENV_AUTO_INSTALL=false
 
+alias tf="terraform"
 alias tfdocs="terraform-docs -c ~/.terraform-docs.yml md ."
 alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" '
