55a363c3ad691eef578df0f5698e2067b573216c
Theo Chatzimichos <tampakrap@gmail.com>
2023-03-03 11:51:48 +0100

    .zsh/plugins/terraform.zsh: add tfdiff and exclude tf lockfile from tfgrep

--- .zsh/plugins/terraform.zsh
+++ .zsh/plugins/terraform.zsh
@@ -2,4 +2,5 @@ export TFENV_AUTO_INSTALL=false
 
 alias tf="terraform"
 alias tfdocs="terraform-docs -c ~/.terraform-docs.yml md ."
-alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" '
+alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" --exclude=".terraform.lock.hcl" '
+alias tfdiff='diff --exclude=".terraform" --exclude=".git" '
