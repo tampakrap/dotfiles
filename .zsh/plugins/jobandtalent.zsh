a20cf571715f3b5981c565e4319a3c766d23ee48
Theo Chatzimichos <tampakrap@gmail.com>
2023-07-04 15:13:03 +0200

    .zsh/plugins/jobandtalent.zsh: various additions
    - Add env var for saml2aws to set shorter AWS_PROFILE env var
    - Add teleport and argo related env vars

--- .zsh/plugins/jobandtalent.zsh
+++ .zsh/plugins/jobandtalent.zsh
@@ -4,12 +4,15 @@ export AIVEN_TOKEN=$(get-token -t aiven)
 export REMOTE_USER=admin
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export SAML2AWS_REGION=$(cat ${HOME}/.aws/default_region)
+export SAML2AWS_PROFILE_SHORT=true
 export CLOUDFLARE_API_TOKEN=$(get-token -t cloudflare)
 export GOPATH=${HOME}/.go
 export GO111MODULE=on
 export GOPRIVATE="github.com/jobandtalent"
 #export BUNDLE_GEM__FURY__IO=$(get-token -t gemfury)
 #export PATH="/usr/local/opt/python@3.11/libexec/bin:/usr/local/opt/libpq/bin:/usr/local/opt/node@16/bin:$PATH"
+export TELEPORT_PROXY=$(get-token -t teleport)
+export ARGOCD_OPTS="--client-crt $HOME/.tsh/keys/$TELEPORT_PROXY/$USERNAME-app/$TELEPORT_PROXY/argo-x509.pem --client-crt-key $HOME/.tsh/keys/$TELEPORT_PROXY/$USERNAME --grpc-web"
 
 source ${HOME}/Repos/various/saml2aws-oh-my-zsh/saml2aws.plugin.zsh
 
