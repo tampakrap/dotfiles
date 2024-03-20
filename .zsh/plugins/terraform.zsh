8ce3aeccb431eeebe28d4ada47aa3365ee962f1b
Theo Chatzimichos <tampakrap@gmail.com>
2024-03-20 11:54:36 +0100

    .zsh/plugins/terraform.zsh: introduce tffmt function

--- .zsh/plugins/terraform.zsh
+++ .zsh/plugins/terraform.zsh
@@ -4,3 +4,7 @@ alias tf="terraform"
 alias tfdocs="terraform-docs -c ~/.terraform-docs.yml md ."
 alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" --exclude=".terraform.lock.hcl" '
 alias tfdiff='diff --exclude=".terraform" --exclude=".git" '
+
+tffmt() {
+    terraform fmt 2>/dev/null || /usr/local/Cellar/tfenv/3.0.0/versions/$(ls -rt /usr/local/Cellar/tfenv/3.0.0/versions | tail -n 1)/terraform fmt
+}
