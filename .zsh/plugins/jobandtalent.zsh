e9a1ec6b54fdf71a451c7c7a2809aa243313ce94
Theo Chatzimichos <tampakrap@gmail.com>
2024-05-20 12:15:38 +0200

    .zsh/plugins/jobandtalent.zsh: various changes
    - set JT_GITHUB_USER
    - set GITHUB_TOKEN
    - remove the argo functions, will be moved to a script

--- .zsh/plugins/jobandtalent.zsh
+++ .zsh/plugins/jobandtalent.zsh
@@ -1,11 +1,13 @@
 export VAULT_GITHUB_API_TOKEN=$(get-token -t github-vault)
 export TF_VAR_github_api_token=$VAULT_GITHUB_API_TOKEN
+export GITHUB_TOKEN=$(get-token -t github)
 export AIVEN_TOKEN=$(get-token -t aiven)
+export CLOUDFLARE_API_TOKEN=$(get-token -t cloudflare)
 export REMOTE_USER=admin
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export SAML2AWS_REGION=$(cat ${HOME}/.aws/default_region)
 export SAML2AWS_PROFILE_SHORT=true
-export CLOUDFLARE_API_TOKEN=$(get-token -t cloudflare)
+export JT_GITHUB_USER=tampakrap-jt
 #export GO111MODULE=on
 #export GOPRIVATE="github.com/jobandtalent"
 #export BUNDLE_GEM__FURY__IO=$(get-token -t gemfury)
@@ -24,23 +26,3 @@ old_saml2aws_login_wrapper() {
 #
 ## The next line enables shell command completion for gcloud.
 #if [ -f '/Users/tampakrap/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/tampakrap/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
-
-connect_argo() {
-    tsh login --proxy=$TELEPORT_PROXY
-    tsh apps login argo
-    argocd login --sso $ARGOCD_SERVER
-}
-
-connect_argo_glb() {
-    export TELEPORT_PROXY=$(get-token -t teleport-glb) #"connect.glb.jobandtalent.cloud"
-    export ARGOCD_SERVER=$(get-token -t argocd-glb) #"argo.connect.glb.jobandtalent.cloud"
-    export ARGOCD_OPTS="--client-crt $HOME/.tsh/keys/$TELEPORT_PROXY/$USER-app/$TELEPORT_PROXY/argo-x509.pem --client-crt-key $HOME/.tsh/keys/$TELEPORT_PROXY/$USER --grpc-web"
-    connect_argo
-}
-
-connect_argo_exp() {
-    export TELEPORT_PROXY=$(get-token -t teleport-exp) #"connect.exp.jobandtalent.cloud"
-    export ARGOCD_SERVER=$(get-token -t argocd-exp) #"argo.connect.exp.jobandtalent.cloud"
-    export ARGOCD_OPTS="--client-crt $HOME/.tsh/keys/$TELEPORT_PROXY/$USER-app/$TELEPORT_PROXY/argo-x509.pem --client-crt-key $HOME/.tsh/keys/$TELEPORT_PROXY/$USER --grpc-web"
-    connect_argo
-}
