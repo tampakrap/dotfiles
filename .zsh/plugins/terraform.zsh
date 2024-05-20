d78ab147bdba5eede59a0859d26536dc0eb93656
Theo Chatzimichos <tampakrap@gmail.com>
2024-05-20 12:36:07 +0200

    .zsh/plugins/terraform.zsh: add alias to add more archs in the lockfile

--- .zsh/plugins/terraform.zsh
+++ .zsh/plugins/terraform.zsh
@@ -5,6 +5,7 @@ alias tf="terraform"
 alias tfdocs="terraform-docs -c ~/.terraform-docs.yml md ."
 alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" --exclude=".terraform.lock.hcl" '
 alias tfdiff='diff --exclude=".terraform" --exclude=".git" '
+alias tfproviderslock='tf providers lock -platform=linux_amd64 -platform=linux_arm64 -platform=darwin_amd64 -platform=darwin_arm64'
 
 tffmt() {
     terraform fmt 2>/dev/null || /usr/local/Cellar/tfenv/3.0.0/versions/$(ls -rt /usr/local/Cellar/tfenv/3.0.0/versions | tail -n 1)/terraform fmt
