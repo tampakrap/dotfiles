fad770b8eed519471aa7d1500d32077c3d84a914
Theo Chatzimichos <tampakrap@gmail.com>
2024-11-04 16:53:53 +0100

    .zsh/plugins/terraform.zsh: add tf15 alias and run tffmt with this

--- .zsh/plugins/terraform.zsh
+++ .zsh/plugins/terraform.zsh
@@ -1,11 +1,12 @@
 export TFENV_AUTO_INSTALL=true
 
 alias tf="terraform"
+alias tf15="TFENV_TERRAFORM_VERSION=latest:^1.5 /opt/homebrew/bin/terraform"
 alias tfdocs="terraform-docs -c ~/.terraform-docs.yml md ."
 alias tfgrep='grep --exclude-dir=".terraform" --exclude-dir=".terragrunt" --exclude-dir=".terragrunt-cache" --exclude-dir=".git" --exclude=".terraform.lock.hcl" '
 alias tfdiff='diff --exclude=".terraform" --exclude=".git" '
 alias tfproviderslock='tf providers lock -platform=linux_amd64 -platform=linux_arm64 -platform=darwin_amd64 -platform=darwin_arm64'
 
 tffmt() {
-    terraform fmt 2>/dev/null || /usr/local/Cellar/tfenv/3.0.0/versions/$(ls -rt /usr/local/Cellar/tfenv/3.0.0/versions | tail -n 1)/terraform fmt
+    terraform fmt 2>/dev/null || tf15 fmt
 }
